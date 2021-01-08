bec89a175a38a9804c2092b6b7418023a37caf8e
jon-myers <jon.myers.sound@gmail.com>
2021-01-08 11:47:10 -0800

    heading up

--- etudes/etude_1.py
+++ etudes/etude_1.py
@@ -35,6 +35,7 @@ def traj_to_absolute(traj):
 
 def hsv_to_freq(hsv, primes, fund, oct=(0, 0, 0)):
     oct = np.array(oct)
+    
     if len(np.shape(hsv)) == 2:
         sub_prod = (primes ** hsv) * (2.0 ** (hsv * oct))
         freq = fund * np.prod(sub_prod, axis=1)
@@ -42,8 +43,9 @@ def hsv_to_freq(hsv, primes, fund, oct=(0, 0, 0)):
         freq = fund * np.prod((primes ** hsv) * (2.0 ** (hsv * oct)))
     return freq
 
-def octave_finder(chord, fund, primes, lims = (60, 50 * 2 ** 5)):
-
+def octave_finder(chord, fund, primes, lims = (50, 50 * (2 ** 5))):
+    """Returns all of the possible octave shifts that would make the notes in
+    the chord audible."""
     bit = np.arange(-4, 4)
     cp = cartesian_product(*(bit for i in range(np.shape(chord)[-1])))
     seive = np.zeros(len(cp), dtype=bool)
@@ -52,38 +54,58 @@ def octave_finder(chord, fund, primes, lims = (60, 50 * 2 ** 5)):
         if np.all(np.min(freq) >= lims[0]) and np.all(np.max(freq) <= lims[1]):
             seive[i] = True
     possible_octs = cp[seive]
+    
     return possible_octs
 
-    """Returns all of the possible octave shifts that would make the notes in
-    the chord audible."""
+def make_chord_sequence(size, fund=200, primes=(3.0, 5.0, 7.0), min_comma=100,
+                        lims=(50, 50*(2**5))):
+    primes = np.array(primes)
+    t = make_random_trajectory(size-1)
+    a = traj_to_absolute(t)
+    c = json.load(open('etudes/chords.json', 'r'))
+    chords = np.array([i['points'] for i in c])
+    roots = [i['roots'] for i in c]
+    indexes = np.random.randint(len(chords), size=size)
+    chords = chords[indexes]
+    oct_shifts = []
+    for i, chord in enumerate(chords):
+        trs = get_transpositions(chord)
+        tr = trs[np.random.choice(len(trs))]
+        chords[i] = tr
+        roots = tr[are_roots(tr)]
+        root_index = np.random.choice(np.arange(len(roots)))
+        root = roots[root_index]
+        diff = a[i] - root
+        chords[i] += diff
+        octs = octave_finder(chords[i], fund, primes, lims)
+        
+        # minimize commas by filtering list of possible octaves even 
+        # further to stop including those whose minimum of combinatorial 
+        # magnitude differences of frequencies is above some threshold. 
+        min_cents = []
+        for oct in octs:
+            freqs = hsv_to_freq(chords[i], primes, fund, oct)
+            combs = itertools.combinations(range(len(freqs)), 2)
+            diffs = [hz_to_cents(freqs[c_[0]], freqs[c_[1]]) for c_ in list(combs)]
+            min = np.min(np.abs(diffs))
+            min_cents.append(min)
+        octs = octs[np.array(min_cents) >= min_comma]
+        
+        # choose between the different octs, randomly
+        oct = octs[np.random.randint(len(octs))]
+        oct_shifts.append(oct)
+    return chords, np.array(oct_shifts)
+    
+        
 
-size = 13
-t = make_random_trajectory(size-1)
-a = traj_to_absolute(t)
-primes = np.array((3.0, 5.0, 7.0))
 fund = 200
-# octs = TODO make octfinder !!
-c = json.load(open('etudes/chords.json', 'r'))
-chords = np.array([i['points'] for i in c])
-roots = [i['roots'] for i in c]
+primes = np.array((3.0, 5.0, 7.0))
+chords, oct_shifts = make_chord_sequence(13, fund)
+all_freq = []
 
-indexes = np.random.randint(len(chords), size=size)
-chords = chords[indexes]
 for i, chord in enumerate(chords):
-    trs = get_transpositions(chord)
-    tr = trs[np.random.choice(len(trs))]
-    chords[i] = tr
-    roots = tr[are_roots(tr)]
-    root = roots[np.random.choice(np.arange(len(roots)))]
-    diff = a[i] - root
-    chords[i] += diff
-
-
-octs = octave_finder(chords[0], 200, primes)
-print(len(octs))
-# freqs = hsv_to_freq(chords[i], primes, fund)
-
-
-#
-# print(chords)
-# print(freqs)
+    freqs = hsv_to_freq(chord, primes, fund, oct_shifts[i])
+    all_freq.append(freqs)
+    
+all_freq = np.round(all_freq, 2)
+json.dump(all_freq, open('etudes/chord_sequence.json', 'w'), cls=NpEncoder)
