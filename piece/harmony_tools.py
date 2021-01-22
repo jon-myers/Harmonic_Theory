59610e9c756eca107bd35a2262b3d4a3431cce12
jon-myers <jon.myers.sound@gmail.com>
2021-01-21 19:13:53 -0800

    just some notes

---
+++ piece/harmony_tools.py
@@ -0,0 +1,10 @@
+# once you have a sequence, or overlay, or harmonic form, ways to articulate
+# those forms.
+
+# All subsets as equally weighted--or unequally weighted--sound events in
+# a gamut. Gamut items presented stochastically, quasi-stochastically (i.e.
+# dc_alg), in some hierarchical subsystem, or monophonic or ... or ... or etc etc
+
+
+# as a monophonic gamut. Gamut items presented stochastically, quasi-stochastically (i.e.
+# dc_alg), in some hierarchical subsystem, or melodic or harmonic form or etc.
